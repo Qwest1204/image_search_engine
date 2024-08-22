cb2794149138680c452f9fe4c9e5d4cef680eff0
Daniil <122814306+Qwest1204@users.noreply.github.com>
2024-08-22 22:54:44 +0300

    feat: first step to realize api

--- api/main_API.py
+++ api/main_API.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import base64
 
 app = FastAPI()
 
+@app.post("/upload_image")
+async def upload_image(image: UploadFile = File(...)):
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    image_data = await image.read()
+
+    image_base64 = base64.b64encode(image_data).decode('utf-8')
+
+    with open("image.png", "wb") as f:
+        f.write(base64.b64decode(image_base64))
+
+    return JSONResponse({"message": "Image received successfully"})
