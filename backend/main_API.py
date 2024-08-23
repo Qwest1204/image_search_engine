6d10123808012fe8203e15610cc0d5e4ba9d102c
Daniil <122814306+Qwest1204@users.noreply.github.com>
2024-08-23 14:07:39 +0300

    feat: add read and add data to system

--- backend/main_API.py
+++ backend/main_API.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI, File, UploadFile
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile, Depends
 from fastapi.responses import JSONResponse
-from main_BACK import add_data_base64
+from main_BACK import add_data_base64, get_data_text
+from pydantic import BaseModel
 import base64
 
 app = FastAPI()
+
+class Item(BaseModel):
+    query: str
+    n_num: int
+
 
 @app.post("/add_data_to_system")
 async def upload_image(image: UploadFile = File(...)):
@@ -11,3 +19,9 @@ async def upload_image(image: UploadFile = File(...)):
     image_base64 = [base64.b64encode(image_data).decode('utf-8')]
     add_data_base64(image_base64)
     return JSONResponse({"message": "Image received successfully"})
+
+
+@app.get("/get_data_from_system_from_text")
+async def get_data_from_system_from_text(query: Annotated[Item, Depends()]):
+    result = get_data_text(query.query, query.n_num)
+    return JSONResponse({"message": result['ids']})
