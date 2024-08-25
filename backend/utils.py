9f30addd47700221f1c8c7e85dd1af3ae727be53
Qwest1204 <workemailfordaniil@gmail.com>
2024-08-25 18:07:59 +0300

    refactor: add comments, functions base64_to_narray_ones, get_uid are obsolete and no longer used, improved connection to the postgre database

--- backend/utils.py
+++ backend/utils.py
@@ -1,16 +1,16 @@
 import os
-
 import hashlib
-import cv2
 import base64
+
 import numpy as np
 from numpy import asarray
 from io import BytesIO
 from PIL import Image
-from CONFIG import *
 import pickle
 
+from CONFIG import DB_CONNECT_KWARGS
 import psycopg2 as psy
+
 import chromadb
 from chromadb import Settings
 from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction
@@ -26,21 +26,50 @@ multimodal_db = client.get_or_create_collection(name="multimodal",
                                                 data_loader=imageloader,
                                                 )
 
-##----------postgresql----------------------------
-DB_CONNECT_KWARGS['host'] = os.environ.get("DATABASE_HOST")
-connection = psy.connect(**DB_CONNECT_KWARGS)
-connection.set_session(autocommit=True)
-cursor = connection.cursor()
+
+def connect_to_postgres(DB_CONNECT_KWARGS):
+    """ Function for connecting to a database, implemented via psycopg2
+
+    Args:
+        DB_CONNECT_KWARGS: Dictionary of database connection parameters. look in CONFIG.py
+
+    Returns:
+        connection: database connection
+    """
+
+    try:
+        DB_CONNECT_KWARGS['host'] = os.environ.get("DATABASE_HOST")
+        connection = psy.connect(**DB_CONNECT_KWARGS)
+        connection.set_session(autocommit=True)
+        return connection
+    except (Exception, psy.Error) as error:
+        print("Error of connection to database: ", error)
+        return None
 
 
 def load_image_into_numpy_array(data):
+    """Loads an image from bytes data into a NumPy array.
+
+    Args:
+        data: Bytes data representing the image.
+
+    Returns:
+        A NumPy array representing the image.
     """
 
-    """
     return np.array(Image.open(BytesIO(data)))
 
 
-def base64_to_narray_list(base64_str) -> list:
+def base64_to_narray(base64_str) -> list:
+    """Converts a list of base64 encoded images to NumPy arrays.
+
+    Args:
+       base64_str: A list of base64 encoded strings.
+
+    Returns:
+        narray_img: A list of NumPy arrays representing the images.
+    """
+
     narray_img = []
     for j in range(len(base64_str)):
         image_bytes = base64.b64decode(base64_str[j])
@@ -49,34 +78,54 @@ def base64_to_narray_list(base64_str) -> list:
     return narray_img
 
 
-def base64_to_narray_ones(base64_str):
-    image_bytes = base64.b64decode(base64_str)
-    image = Image.open(BytesIO(image_bytes))
-    return np.array(image)
+# def base64_to_narray_ones(base64_str):
+#     image_bytes = base64.b64decode(base64_str)
+#     image = Image.open(BytesIO(image_bytes))
+#     return np.array(image)
 
 
 def get_hash_of_base64(base64_str) -> list:
+    """Generates a SHA256 hash for each base64 encoded string.
+
+    Args:
+        base64_str: A list of base64 encoded strings.
+
+    Returns:
+        A list of SHA256 hashes (truncated to 10 characters).
+    """
+
     array_hash = []
     for k in range(len(base64_str)):
         array_hash.append(hashlib.sha256(base64_str[k].encode('utf-8')).hexdigest()[:10])
     return array_hash
 
 
-def get_uid(data) -> list:
-    hash_img = []
-    for i in range(len(data)):
-        with open(data[i], "rb") as f:
-            hash_img.append(hashlib.sha256(f.read()).hexdigest()[:10])
-    return hash_img
+# def get_uid(data) -> list:
+#
+#     hash_img = []
+#     for i in range(len(data)):
+#         with open(data[i], "rb") as f:
+#             hash_img.append(hashlib.sha256(f.read()).hexdigest()[:10])
+#     return hash_img
 
 
 def get_blobs(path):
+    """Reads image data from a file as binary.
+
+    Args:
+        path: The path to the image file.
+
+    Returns:
+        Binary data representing the image.
+    """
+
     drawing = open(path, 'rb').read()
     return psy.Binary(drawing)
 
 
 def get_image_array_from_postgres(uid) -> list:
     some_array = []
+    cursor = connect_to_postgres(DB_CONNECT_KWARGS).cursor()
     for ids in range(len(uid)):
         cursor.execute(
             """
@@ -91,6 +140,7 @@ def get_image_array_from_postgres(uid) -> list:
 
 
 def add_data_to_postgres(data, ids, d_type):
+    cursor = connect_to_postgres(DB_CONNECT_KWARGS).cursor()
     if d_type == 'base64':
         for i in range(len(data)):
             cursor.execute(
@@ -98,7 +148,7 @@ def add_data_to_postgres(data, ids, d_type):
                 INSERT INTO chroma_images(uniq_id, np_array_bytes, image_bytes)
                 VALUES (%s, %s, %s)
                 """,
-                (ids[i], pickle.dumps(base64_to_narray_ones(data[i])), psy.Binary(base64.b64decode(data[i])))
+                (ids[i], pickle.dumps(base64_to_narray([data[i]])[0]), psy.Binary(base64.b64decode(data[i])))
             )
     if d_type == 'image':
         for i in range(len(data)):
@@ -113,7 +163,7 @@ def add_data_to_postgres(data, ids, d_type):
 
 def add_data_base64(base64_string) -> int:
     ids = get_hash_of_base64(base64_string)
-    image_data = base64_to_narray_list(base64_string)
+    image_data = base64_to_narray(base64_string)
     multimodal_db.add(
         ids=ids,
         images=image_data,
@@ -122,14 +172,14 @@ def add_data_base64(base64_string) -> int:
     return multimodal_db.count()
 
 
-def add_data_image(paths) -> int:
-    ids = get_uid(paths)
-    multimodal_db.add(
-        ids=ids,
-        uris=paths,
-    )
-    add_data_to_postgres(paths, ids, d_type='image')
-    return multimodal_db.count()
+# def add_data_image(paths) -> int:
+#     ids = get_uid(paths)
+#     multimodal_db.add(
+#         ids=ids,
+#         uris=paths,
+#     )
+#     add_data_to_postgres(paths, ids, d_type='image')
+#     return multimodal_db.count()
 
 
 def get_data_text(text, n_results):
@@ -159,5 +209,12 @@ def get_data_images(image, n_results):
     }
     return query_dict
 
+
 def get_total_row():
+    """ Return the total number of rows in the database
+
+    Returns:
+        total number of rows in the database
+
+    """
     return multimodal_db.count()
